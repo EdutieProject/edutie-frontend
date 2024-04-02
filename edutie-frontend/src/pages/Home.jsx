e4d5770d84363d34004d2c314c8c53a6fb74217b
sukerberk1 <szymon.l3232@gmail.com>
2024-04-02 19:11:51 +0200

    1st approach on Homepage PART 1

--- edutie-frontend/src/pages/Home.jsx
+++ edutie-frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
-import { Typography, Grid, Box, useTheme } from "@mui/material";
+import { Typography, Grid, Box, useTheme, Button, IconButton } from "@mui/material";
 import NavLayout from "./layout/NavLayout";
-
+import Surface from "../components/Tile";
+import Example from "../components/charts/LineChart";
+import ChevronRight from "@material-ui/icons/ChevronRight"
+import clock from "../assets/img/clock.png"
 
 const suggestedElements = [
     {
@@ -23,20 +26,6 @@ const suggestedElements = [
     }
 ]
 
-function Tile({children}) {
-    const theme = useTheme();
-
-    return(
-    <Box sx={{
-        background: theme.palette.grey[200], //CHANGE TO STH LIKE "SURFACE"
-        padding: theme.spacing(2),
-        borderRadius: 2
-    }}>
-        { children }
-    </Box>
-    )
-}
-
 export default function Home()
 {
     const theme = useTheme();
@@ -46,37 +35,63 @@ export default function Home()
             display: "flex",
             flexDirection: "column",
             gap: theme.spacing(2)
+        },
+        surfaceElementTitleBox: {
+            display: "flex",
+            gap: 2,
+            alignItems: "center"
         }
-    }
+    };
 
     return(
             <NavLayout>                    
-                <Grid container 
-                rowGap={theme.spacing(8)} 
-                >
-                    <Grid item xs={12} padding={theme.spacing(2)}>
-                            <Typography variant="h2"> Cześć! </Typography>
-                            <Typography variant="subtitle1"> Wróć do nauki 🔜 </Typography>
-                            <Tile>
-                                <Typography variant="body">Tutaj powinno być to co ostatnio robiłeś.</Typography>
-                            </Tile>
+                <Grid container rowGap={theme.spacing(4)} paddingY={theme.spacing(4)}>
+                    <Grid item xs={12}>
+                            <Typography variant="h2">Cześć Michał!</Typography>
+                            <Typography variant="subtitle1">Dobrze cię znowu widzieć</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Grid container flexDirection={"column"} gap={theme.spacing(2)}>
+                            <Surface>
+                                <Grid container justifyContent={"space-around"} alignItems={"center"}>
+                                    <Box sx={styles.surfaceElementTitleBox}>
+                                        <img src={clock} width={48} height={48}/>
+                                        <Typography variant="h6">Hello World!</Typography>
+                                    </Box>
+                                    <IconButton>
+                                        <ChevronRight/>
+                                    </IconButton>
+                                </Grid>
+                            </Surface>
+                            <Surface>
+                                <Grid container justifyContent={"space-around"} alignItems={"center"}>
+                                    <Box sx={styles.surfaceElementTitleBox}>
+                                        <img src={clock} width={48} height={48}/>
+                                        <Typography variant="h6">Hello World!</Typography>
+                                    </Box>
+                                    <IconButton>
+                                        <ChevronRight color="primary"/>
+                                    </IconButton>
+                                </Grid>
+                            </Surface>
+                        </Grid>
                     </Grid> 
-                    <Grid item xs={12} sm={6} padding={theme.spacing(2)}>
-                        <Typography variant="h3" marginY={theme.spacing(2)}>
-                            Sugestie dla ciebie:
+                    <Grid item xs={12} sm={6}>
+                        <Typography variant="h4" marginY={theme.spacing(2)}>
+                            Wybrane dla ciebie:
                         </Typography>
                         <Box sx={styles.suggestionBox}>
                         {
                             suggestedElements.map((item) =>
-                                <Tile>
+                                <Surface>
                                     <Typography>{item.title}</Typography>
-                                </Tile>
+                                </Surface>
                             )
                         }
                         </Box>
                     </Grid>
-                    <Grid item xs={12} sm={6} padding={theme.spacing(2)}>
-                        <Typography>Your stats</Typography>
+                    <Grid item xs={12} sm={6}>
+                        <Example/>
                     </Grid>
                 </Grid>
             </NavLayout>
