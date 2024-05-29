fd0187f9340bbc5284f966648aec29bee747579e
michalinapepel <misia.pepel@onet.pl>
2024-05-30 00:05:24 +0200

    Edit Surface component border radius

--- edutie-frontend/src/components/global/Surface.jsx
+++ edutie-frontend/src/components/global/Surface.jsx
@@ -1,20 +1,16 @@
 import { Box, styled } from "@mui/material";
 
 const StyledBox = styled(Box)(({ theme }) => ({
-    background: theme.palette.surface.main,
-    padding: theme.spacing(2),
-    borderRadius: 4,
-}))
+  background: theme.palette.surface.main,
+  padding: theme.spacing(2),
+  borderRadius: 20,
+}));
 
 /**
  * A surface-like component providing a rounded tile to encompass content.
- * @param children Component's children 
+ * @param children Component's children
  * @returns React Component
  */
 export default function Surface({ children, sx }) {
-    return (
-        <StyledBox sx={{ boxShadow: 1, ...sx }}>
-            {children}
-        </StyledBox>
-    );
+  return <StyledBox sx={{ boxShadow: 1, ...sx }}>{children}</StyledBox>;
 }
