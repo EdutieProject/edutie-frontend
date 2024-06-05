097f1277018e8bff10c17f8111a6eac8c9dfeda0
sukerberk1 <szymon.l3232@gmail.com>
2024-06-05 23:08:03 +0200

    Add segment button to playground!

--- edutie-frontend/src/views/PlaygroundView.jsx
+++ edutie-frontend/src/views/PlaygroundView.jsx
@@ -1,12 +1,19 @@
 import { Button, Typography } from "@mui/material";
 import NavLayout from "./layout/NavLayout";
-import { getCourses } from "../services/studyProgramLearningService";
+import { getCourses, getLessons, getSciences, getSegments } from "../services/studyProgramLearningService";
 
 export default function PlaygroundView() {
     return (
         <NavLayout>
             <Typography>Hello World! This view is currently occupied for API testing purposes</Typography>
-            <Button onClick={()=>getCourses("db48e4f2-e385-4bf4-9e77-9e6939472529").then(o=>console.log(o))}>Fetch</Button>
+            <Button onClick={()=>getSciences().then(o=>console.log(o))}>Tutaj zaloguj do konsoli dziedziny - matematyka etc.</Button>
+            <Button onClick={
+                ()=>getSciences().then(o=>getCourses(o.data[0].id)
+                .then(c=>getLessons(c.data[0].id)
+                .then(x=>getSegments(x.data[0].lesson.id)
+                .then(segments => console.log(segments)
+                ))))
+                }>Tutaj zaloguj do konsoli segmenty - payload dla drzewek segmentów</Button>
         </NavLayout>
     )
 }
