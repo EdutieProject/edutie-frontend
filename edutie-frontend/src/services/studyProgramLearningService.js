5b362e64f510a56eca32633679af5b838b5b7f07
michal-lesniowski <kevin@januszsoft.pl>
2024-06-10 18:18:09 +0200

    Revert "Change service functions"
    This reverts commit d23b20d2a52fe37c5ca7bfc47fd988b3d76e5590.

--- edutie-frontend/src/services/studyProgramLearningService.js
+++ edutie-frontend/src/services/studyProgramLearningService.js
@@ -1,46 +1,26 @@
-import { LEARNING_API, catchClientErrors, defaultHeaders } from "./apiCommons";
+import { LEARNING_API, defaultHeaders } from "./apiConfig";
 
 export async function getSciences() {
-    return await catchClientErrors(
-        async () => {
-            const response = await fetch(`${LEARNING_API}/sciences`, { method: "GET", headers: defaultHeaders });
-            return await response.json();
-        }
-    );
+    const response = await fetch(`${LEARNING_API}/sciences`, { method: "GET", headers: defaultHeaders });
+    return await response.json();
 }
 
 export async function getCourses(scienceId) {
-    return await catchClientErrors(
-        async () => {
-            const response = await fetch(`${LEARNING_API}/courses?scienceId=${scienceId}`, { method: "GET", headers: defaultHeaders })
-            return await response.json();
-        }
-    );
+    const response = await fetch(`${LEARNING_API}/courses?scienceId=${scienceId}`, { method: "GET", headers: defaultHeaders })
+    return await response.json();
 }
 
 export async function getProgressedCourses() {
-    return await catchClientErrors(
-        async () => {
-            const response = await fetch(`${LEARNING_API}/courses/progressed`, { method: "GET", headers: defaultHeaders })
-            return await response.json();
-        }
-    );
+    const response = await fetch(`${LEARNING_API}/courses/progressed`, { method: "GET", headers: defaultHeaders })
+    return await response.json();
 }
 
 export async function getLessons(courseId) {
-    return await catchClientErrors(
-        async () => {
-            const response = await fetch(`${LEARNING_API}/lessons?courseId=${courseId}`, { method: "GET", headers: defaultHeaders });
-            return await response.json();
-        }
-    );
+    const response = await fetch(`${LEARNING_API}/lessons?courseId=${courseId}`, { method: "GET", headers: defaultHeaders });
+    return await response.json();
 }
 
 export async function getSegments(lessonId) {
-    return await catchClientErrors(
-        async () => {
-            const response = await fetch(`${LEARNING_API}/segments?lessonId=${lessonId}`, { method: "GET", headers: defaultHeaders });
-            return await response.json();
-        }
-    );
+    const response = await fetch(`${LEARNING_API}/segments?lessonId=${lessonId}`, { method: "GET", headers: defaultHeaders });
+    return await response.json();
 }
