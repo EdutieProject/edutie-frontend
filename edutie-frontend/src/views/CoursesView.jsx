89ca7c2af5b3faf69746031925bedf0e26b1b2a6
sukerberk1 <szymon.l3232@gmail.com>
2024-08-29 23:39:37 +0200

    Courses View

--- edutie-frontend/src/views/CoursesView.jsx
+++ edutie-frontend/src/views/CoursesView.jsx
@@ -20,6 +20,8 @@ import {
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import LoadingView from "./common/LoadingView";
 import Heading from "../components/global/Heading";
+import CircleButton from "../components/global/CircleButton";
+import UserIcon from "../components/customIcons/UserIcon";
 
 export default function CoursesView() {
   const theme = useTheme();
@@ -27,6 +29,9 @@ export default function CoursesView() {
   const [isLoading, setIsLoading] = useState(true);
   let sciencesData = useRef([]);
   const [selectedScienceIndex, setSelectedScienceIndex] = useState(0);
+
+  /** A state broker is here - transferring state between CourseList and higher-level view component  */
+  const [selectedTags, setSelectedTags] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -50,16 +55,17 @@ export default function CoursesView() {
 
 
   let sciences = sciencesData.current;
+  let selectedScience = sciences[selectedScienceIndex];
   return (
     <NavLayout mode="flex">
-      <Grid container direction="row" justifyContent="space-between">
-        <CourseList scienceId={sciences[selectedScienceIndex].id} />
-        <Grid>
+      <Grid container direction="row" justifyContent="space-between" gap={theme.spacing(14)}>
+        <CourseList scienceId={selectedScience.id} />
+        <Grid sx={{flexGrow: 1}}>
           <Grid
             container
             item
             direction="row"
-            justifyContent="space-between"
+            justifyContent="space-around"
             alignItems="center"
             marginBottom="20px"
           >
@@ -73,7 +79,7 @@ export default function CoursesView() {
             >
               <ChevronLeft />
             </IconButton>
-            <Typography fontSize="large">{sciences[selectedScienceIndex].name}</Typography>
+            <Heading variant="h5">{selectedScience.name}</Heading>
             <IconButton
               sx={{ color: "black" }}
               onClick={() => {
@@ -85,11 +91,14 @@ export default function CoursesView() {
               <ChevronRight />
             </IconButton>
           </Grid>
+          <Box sx={{display: "grid", placeItems: "center"}}>
           <img
-            src=".\src\assets\img\ExampleImage.webp"
+            src={selectedScience.imageSource === null ? "./src/assets/img/ExampleImage.webp" : selectedScience.imageSource}
             alt="Science Picture"
             width={250}
+            style={{aspectRatio: 1/1, objectFit: "cover", borderRadius: 20}}
           />
+          </Box>
         </Grid>
       </Grid>
     </NavLayout>
@@ -134,36 +143,28 @@ function CourseList({ scienceId }) {
           .slice((page - 1) * 3, page * 3)
           .map((course, index) => <CourseTile course={course} key={index} />)
       }
-      {filteredCourses.length > 3 ? (
         <Pagination
           count={Math.ceil(filteredCourses.length / 3)}
           onChange={(e, value) => setPage(value)}
         />
-      ) : (
-        <Typography color={theme.palette.surface.main[200]}>
-          To koniec wyszukiwań...
-        </Typography>
-      )}
     </Grid>
   );
 }
 
 const CourseTile = ({ course }) => {
   const theme = useTheme();
-
-  console.log(course);
   return (
     <Surface sx={{ my: theme.spacing(4), display: "flex", gap: theme.spacing(4) }}>
-      <Box width={"18%"}>
+      <Box width={"15%"}>
         <img src={course.imageSource === null ?
           "https://thumbs.dreamstime.com/b/trigonometry-formula-line-icon-vector-illustration-sign-isolated-contour-symbol-black-331770196.jpg"
           : course.imageSource}
           width="100%"
         />
       </Box>
-      <Box flexGrow={1}>
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
         <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-          <Heading variant={"h3"}>{course.name}</Heading>
+          <Heading variant={"h4"}>{course.name}</Heading>
           <Box sx={{ display: "flex", flexDirection: "row", gap: theme.spacing(2) }}>
             {course.courseTags.map(tag =>
               <Box sx={{ borderRadius: 25, backgroundColor: theme.palette.secondary.main, px: theme.spacing(2), py: theme.spacing(1), display: "grid", placeItems: "center" }}>
@@ -175,6 +176,16 @@ const CourseTile = ({ course }) => {
         <Typography variant="body1">
           {course.description}
         </Typography>
+        <Box sx={{ mt: theme.spacing(1), display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+          <Box sx={{ display: "grid", placeItems: "center" }}>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: theme.spacing(2) }}>
+              <UserIcon color={theme.palette.common.black} /> Author goes here
+            </Box>
+          </Box>
+          <CircleButton size={theme.spacing(3)}>
+            <Typography fontFamily={"Baloo"} fontSize={24} color={theme.palette.common.white}>{">"}</Typography>
+          </CircleButton>
+        </Box>
       </Box>
     </Surface>
   );
