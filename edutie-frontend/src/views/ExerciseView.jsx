e89b9af37c8c2554ad8860822062583e87722322
sukerberk1 <szymon.l3232@gmail.com>
2024-08-31 15:35:09 +0200

    Fix hint cards

--- edutie-frontend/src/views/ExerciseView.jsx
+++ edutie-frontend/src/views/ExerciseView.jsx
@@ -13,8 +13,9 @@ export default function ExcerciseView() {
   const theme = useTheme();
   const { resourceId } = useParams();
   const { state } = useLocation();
-  const { learningResource } = state;
+  const learningResource = state;
 
+  console.log(state);
 
   const Views = Object.freeze({ THEORY: useEnumValue("THEORY"), ACTIVITY: useEnumValue("ACTIVITY") })
   const [currentView, setCurrentView] = useState(Views.THEORY);
@@ -35,9 +36,9 @@ export default function ExcerciseView() {
       </Box>
       {
         currentView == Views.THEORY ?
-          <TheoryBlock></TheoryBlock> :
+          <TheoryBlock theory={learningResource.theory} /> :
           currentView == Views.ACTIVITY ?
-            <ActivityBlock></ActivityBlock> :
+            <ActivityBlock activity={learningResource.activity} /> :
             <>Critical Error! {/** TODO: Error messages */} </>
       }
     </NavLayout>
@@ -65,28 +66,19 @@ function TheoryLayout({ children }) {
   )
 }
 
-function TheoryBlock() {
+function TheoryBlock({ theory }) {
   const theme = useTheme();
   return (
     <TheoryLayout>
       <Surface sx={{ gridArea: "left" }}>
         <Typography fontFamily={"Baloo"} variant='h5' marginY={theme.spacing(2)}>Czym są elektrowstrząsy?</Typography>
         <Typography variant='body1'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non nulla consequat, tincidunt libero vel, auctor ex. Proin sem neque, tincidunt sed felis non, aliquam cursus velit. Proin nec luctus erat. Sed ante nunc, consectetur vitae ornare sed, feugiat in sapien. Praesent porta nulla sit amet ante molestie efficitur. Nullam non ipsum lobortis, dignissim leo a, scelerisque leo. Donec semper quis massa gravida luctus. Nunc arcu mauris, ultrices id aliquam ut, tincidunt in enim. Duis a consequat dolor, sit amet fringilla quam.
-          <br /><br />
-          Sed malesuada purus non accumsan congue. In ac elit sit amet quam lacinia scelerisque nec sed quam. Fusce tempor ac sapien ornare porttitor. Nulla id consequat enim, ut tincidunt dui. Sed tempus, felis nec blandit congue, nibh massa sagittis odio, sit amet varius lacus ligula sit amet dui. Nullam porta, sapien at feugiat fermentum, urna ipsum aliquam neque, a efficitur neque nibh sed sapien. Nullam scelerisque in velit non cursus. Integer sed dictum massa. Nullam nunc nunc, sollicitudin et eleifend consectetur, sagittis non enim. Nullam sit amet sapien enim.
-          <br /><br />
-          Duis fringilla fermentum ipsum eu tincidunt. Phasellus et semper orci. Vivamus quis dui commodo, aliquam ligula ac, malesuada ante. Etiam interdum dolor non tempor dignissim. Phasellus fringilla eleifend magna a facilisis. Integer ipsum purus, consectetur ac tellus vel, euismod vestibulum libero.
+          {theory.overview}
         </Typography>
       </Surface>
       <Surface sx={{ gridArea: "right" }}>
         <Typography fontFamily={"Baloo"} variant='h5' marginY={theme.spacing(2)}>Summary</Typography>
-        <ul>
-          <li>Hello</li>
-          <li>World</li>
-          <li>Galaxy</li>
-          <li>Universe</li>
-        </ul>
+        {theory.summary}
       </Surface>
       <Surface sx={{ gridArea: "bottom" }}>
         <Grid container spacing={theme.spacing(2)}>
@@ -94,10 +86,10 @@ function TheoryBlock() {
             <Typography fontFamily={"Baloo"} variant='h4' marginY={theme.spacing(2)}>Twój zeszyt</Typography>
           </Grid>
           <Grid item xs={4}>
-            Hello
+            <i>TBA...</i>
           </Grid>
           <Grid item xs={4}>
-            Wrld
+            Coming soon
           </Grid>
         </Grid>
 
@@ -126,14 +118,14 @@ function ActivityLayout({ children }) {
   )
 }
 
-function ActivityBlock() {
+function ActivityBlock({ activity }) {
   const theme = useTheme();
   return (
     <ActivityLayout>
       <Surface sx={{ gridArea: "left" }}>
         <Typography fontFamily={"Baloo"} variant='h5' marginY={theme.spacing(2)}>Zadanie dla Ciebie</Typography>
         <Typography variant='body1'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non nulla consequat, tincidunt libero vel, auctor ex. Proin sem neque, tincidunt sed felis non, aliquam cursus velit. Proin nec luctus erat. Sed ante nunc, consectetur vitae ornare sed, feugiat in sapien. Praesent porta nulla sit amet ante molestie efficitur. Nullam non ipsum lobortis, dignissim leo a, scelerisque leo. Donec semper quis massa gravida luctus. Nunc arcu mauris, ultrices id aliquam ut, tincidunt in enim. Duis a consequat dolor, sit amet fringilla quam.
+          {activity.activityText}
         </Typography>
       </Surface>
       <Surface sx={{ gridArea: "right" }}>
@@ -141,13 +133,15 @@ function ActivityBlock() {
         <Typography variant='body1'>Opisz swoje rozwiązanie. Posłuż się przygotowanym do tego szablonem</Typography>
         <TextField multiline fullWidth sx={{ backgroundColor: theme.palette.common.white, outline: "none", border: "none", borderRadius: 10, marginY: theme.spacing(4), paddingY: theme.spacing(2), "& fieldset": { border: 'none' }, }} minRows={8} maxRows={16} />
       </Surface>
-      <Surface sx={{ gridArea: "bottom", display: "flex", justifyContent: "space-between", flexWrap: "wrap" }}>
+      <Surface sx={{ gridArea: "bottom", display: "flex", justifyContent: "space-between", flexWrap: "nowrap" }}>
         <Typography fontFamily={"Baloo"} variant='h4' marginY={theme.spacing(2)}>Użyj podpowiedzi</Typography>
-        <Box sx={{ display: "flex", justifyContent: "flex-end", gap: theme.spacing(2) }}>
-          <HintTile hintText={"World"} />
-          <HintTile hintText={"Hello"} />
-          <HintTile hintText={"Universe"} />
-        </Box>
+        <Grid container gap={theme.spacing(2)} justifyContent={"flex-end"}>
+          {
+            activity.hints.map(
+              (hint, i) => <HintTile hintText={hint.text} key={i} />
+            )
+          }
+        </Grid>
       </Surface>
       <Box sx={{ gridArea: "button", display: "flex", alignItems: "center", justifyContent: "center" }}>
         <CircleButton size={theme.spacing(7)} onClick={() => console.log("Button click!")}>
@@ -165,22 +159,26 @@ function HintTile({ hintText }) {
 
   if (revealed === false)
     return (
-      <Surface sx={{
-        backgroundColor: theme.palette.secondary.main,
-        flex: "0 0 auto",
-        aspectRatio: "5/3",
-        display: "grid",
-        placeItems: "center"
-      }}
-        onClick={() => setRevealed(true)}
-      >
-        <TurnAroundIcon />
-      </Surface>
+      <Grid item xs={3}>
+        <Surface sx={{
+          backgroundColor: theme.palette.secondary.main,
+          flex: "0 0 auto",
+          aspectRatio: "5/3",
+          display: "grid",
+          placeItems: "center"
+        }}
+          onClick={() => setRevealed(true)}
+        >
+          <TurnAroundIcon />
+        </Surface>
+      </Grid>
     );
 
   return (
-    <Surface sx={{ backgroundColor: theme.palette.common.white, flex: "0 0 auto", aspectRatio: "5/3" }}>
-      <Typography variant='body1'>{hintText}</Typography>
-    </Surface>
+    <Grid item xs={3}>
+      <Surface sx={{ backgroundColor: theme.palette.common.white, flex: "0 0 auto", aspectRatio: "5/3", textWrap: "wrap" }}>
+        <Typography variant='body1'>{hintText}</Typography>
+      </Surface>
+    </Grid>
   );
 }
