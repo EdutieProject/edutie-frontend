7aead310f7801fc88cf7caef6bafa08aa12875d1
michalinapepel <misia.pepel@onet.pl>
2024-09-05 12:38:04 +0200

    Buttons changing colors onclick

--- edutie-frontend/src/views/LRDCreationView.jsx
+++ edutie-frontend/src/views/LRDCreationView.jsx
@@ -3,9 +3,24 @@ import NavLayout from "./layout/NavLayout";
 import Surface from "../components/global/Surface";
 import RoundedButton from "../components/global/RoundedButton.jsx";
 import CircleButton from "../components/global/CircleButton.jsx";
+import { useState } from "react";
 
 export default function LRDCreationView() {
   const theme = useTheme();
+  const [buttonClicked, setButtonClicked] = useState("firstButton");
+  const [firstButtonColor, setFirstButtonColor] = useState(
+    theme.palette.surface
+  );
+  const [secondButtonColor, setSecondButtonColor] = useState(
+    theme.palette.surface
+  );
+  const [thirdButtonColor, setThirdButtonColor] = useState(
+    theme.palette.surface
+  );
+  const [fourthButtonColor, setFourthButtonColor] = useState(
+    theme.palette.surface
+  );
+
   return (
     <NavLayout mode="flex">
       <Typography fontFamily={"Baloo"} variant="h4">
@@ -59,6 +74,7 @@ export default function LRDCreationView() {
             <Surface sx={{ height: "10vh" }}></Surface>
           </Grid>
         </Grid>
+
         <Grid sx={{ marginBottom: theme.spacing(2) }}>
           <Typography fontFamily={"Baloo"} variant="h6">
             Opisz szczegółowo, na czym będzie polegało dane zadanie i czego
@@ -66,9 +82,17 @@ export default function LRDCreationView() {
           </Typography>
         </Grid>
         <Grid container>
-          <Grid xs item>
+          <InputSurface buttonClicked={buttonClicked}></InputSurface>
+          {/* <Grid xs item>
             <Surface>
-              <Grid item sx={{ height: "30vh" }}></Grid>
+              <Grid item sx={{ height: "30vh" }}>
+                <TextField
+                  label="Wprowadź odpowiedź"
+                  multiline
+                  fullWidth
+                  maxRows={6}
+                />
+              </Grid>
               <Grid
                 container
                 sx={{
@@ -82,7 +106,7 @@ export default function LRDCreationView() {
                 ></RoundedButton>
               </Grid>
             </Surface>
-          </Grid>
+          </Grid> */}
           <Grid
             container
             direction="column"
@@ -93,16 +117,60 @@ export default function LRDCreationView() {
             xs={1}
           >
             <Grid item sx={{ marginBottom: theme.spacing(1) }}>
-              <CircleButton size={1}></CircleButton>
+              <CircleButton
+                onClick={() => {
+                  setButtonClicked("firstButton");
+                  setSecondButtonColor(theme.palette.surface);
+                  setFirstButtonColor("");
+                  setFourthButtonColor(theme.palette.surface);
+                  setThirdButtonColor(theme.palette.surface);
+                }}
+                bgColor={firstButtonColor}
+                id="firstButton"
+                size={1}
+              ></CircleButton>
             </Grid>
             <Grid item sx={{ marginBottom: theme.spacing(1) }}>
-              <CircleButton size={1}></CircleButton>
+              <CircleButton
+                onClick={() => {
+                  setButtonClicked("secondButton");
+                  setSecondButtonColor("");
+                  setFirstButtonColor(theme.palette.surface);
+                  setFourthButtonColor(theme.palette.surface);
+                  setThirdButtonColor(theme.palette.surface);
+                }}
+                bgColor={secondButtonColor}
+                id="secondButton"
+                size={1}
+              ></CircleButton>
             </Grid>
             <Grid item sx={{ marginBottom: theme.spacing(1) }}>
-              <CircleButton size={1}></CircleButton>
+              <CircleButton
+                onClick={() => {
+                  setButtonClicked("thirdButton");
+                  setSecondButtonColor(theme.palette.surface);
+                  setFirstButtonColor(theme.palette.surface);
+                  setFourthButtonColor(theme.palette.surface);
+                  setThirdButtonColor("");
+                }}
+                bgColor={thirdButtonColor}
+                id="thirdButton"
+                size={1}
+              ></CircleButton>
             </Grid>
             <Grid item sx={{ marginBottom: theme.spacing(1) }}>
-              <CircleButton size={1}></CircleButton>
+              <CircleButton
+                onClick={() => {
+                  setButtonClicked("fourthButton");
+                  setSecondButtonColor(theme.palette.surface);
+                  setFirstButtonColor(theme.palette.surface);
+                  setFourthButtonColor("");
+                  setThirdButtonColor(theme.palette.surface);
+                }}
+                bgColor={fourthButtonColor}
+                id="fourthButton"
+                size={1}
+              ></CircleButton>
             </Grid>
           </Grid>
         </Grid>
@@ -110,3 +178,34 @@ export default function LRDCreationView() {
     </NavLayout>
   );
 }
+
+const InputSurface = ({ buttonClicked }) => {
+  const theme = useTheme();
+  console.log(buttonClicked);
+  return (
+    <Grid xs item>
+      <Surface>
+        <Grid item sx={{ height: "30vh" }}>
+          <TextField
+            label="Wprowadź odpowiedź"
+            multiline
+            fullWidth
+            maxRows={6}
+          />
+        </Grid>
+        <Grid
+          container
+          sx={{
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <RoundedButton
+            label={"Zapisz i przejdź dalej"}
+            active={true}
+          ></RoundedButton>
+        </Grid>
+      </Surface>
+    </Grid>
+  );
+};
