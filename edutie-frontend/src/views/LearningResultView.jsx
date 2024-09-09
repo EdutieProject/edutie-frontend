a2709d17c962b9cffecc9021a96e3ac0a1a71d4c
sukerberk1 <szymon.l3232@gmail.com>
2024-09-09 14:25:59 +0200

    Screen implementation

--- edutie-frontend/src/views/LearningResultView.jsx
+++ edutie-frontend/src/views/LearningResultView.jsx
@@ -1,9 +1,12 @@
 import { useLocation, useParams } from "react-router-dom";
 import NavLayout from "./layout/NavLayout";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getLearningResultById } from "../services/LearningService";
 import ErrorView from "./common/ErrorView";
+import JoyColorfulFaceIcon from "../components/customIcons/JoyColorfulFaceIcon";
+import SadColorfulFaceIcon from "../components/customIcons/SadColorfulFaceIcon";
+import NormalColorfulFaceIcon from "../components/customIcons/NormalColorfulFaceIcon";
 
 
 export default function LearningResultView() {
@@ -18,17 +21,35 @@ export default function LearningResultView() {
             return;
         getLearningResultById(resultId)
             .then(learningResultResponse => {
+                console.log(learningResultResponse)
                 setLearningResult(learningResultResponse.data);
                 setError(learningResultResponse.error);
             });
     }, []);
 
+    const getHeading = (feedbackType) => feedbackType === "POSITIVE" ? "Świetnie!" : feedbackType === "NEGATIVE" ? "Słabo..." : "W porządku.";
+
+    const iconSize = "18rem";
+    const getIcon = (feedbackType) => feedbackType === "POSITIVE" ? <JoyColorfulFaceIcon width={iconSize} height={iconSize} />
+        : feedbackType === "NEGATIVE" ? <SadColorfulFaceIcon width={iconSize} height={iconSize} />
+            : <NormalColorfulFaceIcon width={iconSize} height={iconSize} />;
+
     if (error)
-        return <ErrorView error={error}/>
+        return <ErrorView error={error} />
 
     return (
         <NavLayout mode="flex">
-
+            <Box sx={{ flexGrow: 1, display: "grid", placeItems: "center", transform: "translateY(-25%)" }}>
+                <Box sx={{ display: "flex", flexDirection: "row", gap: theme.spacing(12) }}>
+                    { getIcon(learningResult.feedback.type) }
+                    <Box maxWidth={"36rem"}>
+                        <Typography variant="h3" color="grey">
+                            {getHeading(learningResult.feedback.type)}
+                        </Typography>
+                        <Typography padding={"1em 0"} variant="body1" color="initial">{learningResult.feedback.text}</Typography>
+                    </Box>
+                </Box>
+            </Box>
         </NavLayout>
     );
 }
