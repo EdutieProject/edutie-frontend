90a5654068d39ae032e95fff2a5170689dfc48aa
sukerberk1 <szymon.l3232@gmail.com>
2024-09-17 22:57:11 +0200

    Learning result minor enhancement & workaround

--- edutie-frontend/src/views/LearningResultView.jsx
+++ edutie-frontend/src/views/LearningResultView.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useParams } from "react-router-dom";
 import NavLayout from "./layout/NavLayout";
-import { Box, LinearProgress, Typography, useTheme } from "@mui/material";
+import { Box, Grid, LinearProgress, Typography, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getLearningResultById } from "../services/LearningService";
 import ErrorView from "./common/ErrorView";
@@ -8,7 +8,6 @@ import JoyColorfulFaceIcon from "../components/customIcons/JoyColorfulFaceIcon";
 import SadColorfulFaceIcon from "../components/customIcons/SadColorfulFaceIcon";
 import NormalColorfulFaceIcon from "../components/customIcons/NormalColorfulFaceIcon";
 import LoadingView from "./common/LoadingView";
-import Markdown from "react-markdown";
 import MarkdownLaTeXRenderer from "../components/markdown/MarkdownLaTexRenderer";
 
 
@@ -34,7 +33,7 @@ export default function LearningResultView() {
 
     const getHeading = (feedbackType) => feedbackType === "POSITIVE" ? "Świetnie!" : feedbackType === "NEGATIVE" ? "Słabo..." : "W porządku.";
 
-    const iconSize = "18rem";
+    const iconSize = "24rem";
     const getIcon = (feedbackType) => feedbackType === "POSITIVE" ? <JoyColorfulFaceIcon width={iconSize} height={iconSize} />
         : feedbackType === "NEGATIVE" ? <SadColorfulFaceIcon width={iconSize} height={iconSize} />
             : <NormalColorfulFaceIcon width={iconSize} height={iconSize} />;
@@ -47,55 +46,55 @@ export default function LearningResultView() {
 
     return (
         <NavLayout mode="flex" scroll>
-            <Box sx={{ flexGrow: 1, display: "grid", placeItems: "center" }}>
-                <Box sx={{ display: "flex", flexDirection: "row", gap: theme.spacing(12) }}>
-                    <Box sx={{ display: "grid", placeItems: "center", transform: "translateY(-10%)" }}>
-                        {getIcon(learningResult.feedback.type)}
+            <Grid container flexGrow={1}>
+                <Grid item xs={6} >
+                    <Box sx={{position: "fixed", display: "grid", placeItems: "center", height: "100vh", width: "40%"}}>
+                        <Box sx={{ transform: "translateY(-20%)" }}>{getIcon(learningResult.feedback.type)}</Box>
                     </Box>
-                    <Box maxWidth={"36rem"}>
-                        <Typography variant="h3" color="grey">
-                            {getHeading(learningResult.feedback.type)}
-                        </Typography>
-                        <MarkdownLaTeXRenderer content={learningResult.feedback.text} />
-                        {
-                            learningResult.assessments.map(assessment => {
-                                let learningReq = learningResult.learningResourceDefinition.learningRequirements.filter(o => o.id === assessment.learningRequirementId)[0];
+                </Grid>
+                <Grid item xs={6} sx={{ padding: theme.spacing(4) }}>
+                    <Typography variant="h3" color="grey">
+                        {getHeading(learningResult.feedback.type)}
+                    </Typography>
+                    <MarkdownLaTeXRenderer content={learningResult.feedback.text} />
+                    {
+                        learningResult.assessments.map(assessment => {
+                            let learningReq = learningResult.learningResourceDefinition.learningRequirements.filter(o => o.id === assessment.learningRequirementId)[0];
 
-                                let qualifiedSubReqs = assessment.qualifiedSubRequirements.length;
-                                let allSubReqs = learningReq.subRequirements.length;
-                                return (
-                                    <Box sx={{ marginTop: theme.spacing(4) }}>
-                                        <Typography variant="h5">{learningReq.name}</Typography>
-                                        <Box sx={{ display: "flex", gap: theme.spacing(2) }}>
-                                            <Typography variant="h4" sx={{ display: "grid", placeItems: "center" }}>Ocena: {assessment.grade}</Typography>
-                                            <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-                                                <LinearProgress
-                                                    color="primary"
-                                                    variant="determinate"
-                                                    value={assessment.grade / 6 * 100}
-                                                    sx={{ width: "100%", height: 10, borderRadius: theme.shape.borderRadius }}
-                                                />
-                                            </Box>
+                            let qualifiedSubReqs = assessment.qualifiedSubRequirements.length;
+                            let allSubReqs = learningReq.subRequirements.length;
+                            return (
+                                <Box sx={{ marginTop: theme.spacing(4) }}>
+                                    <Typography variant="h5">{learningReq.name}</Typography>
+                                    <Box sx={{ display: "flex", gap: theme.spacing(2) }}>
+                                        <Typography variant="h4" sx={{ display: "grid", placeItems: "center" }}>Ocena: {assessment.grade}</Typography>
+                                        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+                                            <LinearProgress
+                                                color="primary"
+                                                variant="determinate"
+                                                value={assessment.grade / 6 * 100}
+                                                sx={{ width: "100%", height: 10, borderRadius: theme.shape.borderRadius }}
+                                            />
                                         </Box>
-                                        <Box sx={{ display: "flex", gap: theme.spacing(2) }}>
-                                            <Typography variant="h4" sx={{ display: "grid", placeItems: "center" }}>Trudność: {qualifiedSubReqs / allSubReqs * 100}%</Typography>
-                                            <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-                                                <LinearProgress
-                                                    color="secondary"
-                                                    variant="determinate"
-                                                    value={qualifiedSubReqs / allSubReqs * 100}
-                                                    sx={{ width: "100%", height: 10, borderRadius: theme.shape.borderRadius }}
-                                                />
-                                            </Box>
-                                        </Box>
-                                        <MarkdownLaTeXRenderer content={assessment.feedbackText} />
                                     </Box>
-                                )
-                            })
-                        }
-                    </Box>
-                </Box>
-            </Box>
+                                    <Box sx={{ display: "flex", gap: theme.spacing(2) }}>
+                                        <Typography variant="h4" sx={{ display: "grid", placeItems: "center" }}>Trudność: {qualifiedSubReqs / allSubReqs * 100}%</Typography>
+                                        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+                                            <LinearProgress
+                                                color="secondary"
+                                                variant="determinate"
+                                                value={qualifiedSubReqs / allSubReqs * 100}
+                                                sx={{ width: "100%", height: 10, borderRadius: theme.shape.borderRadius }}
+                                            />
+                                        </Box>
+                                    </Box>
+                                    <MarkdownLaTeXRenderer content={assessment.feedbackText} />
+                                </Box>
+                            )
+                        })
+                    }
+                </Grid>
+            </Grid>
         </NavLayout>
     );
 }
