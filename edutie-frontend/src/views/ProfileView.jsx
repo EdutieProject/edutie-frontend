929305d8ae53503b17d6ca145321654c674d3c7c
sukerberk1 <szymon.l3232@gmail.com>
2024-09-22 20:28:11 +0200

    Add learning result list

--- edutie-frontend/src/views/ProfileView.jsx
+++ edutie-frontend/src/views/ProfileView.jsx
@@ -1,34 +1,41 @@
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Divider, Grid, Typography, useTheme } from "@mui/material";
 import NavLayout from "./layout/NavLayout";
 import useEnumValue from "../hooks/alternative/useEnumValue";
-import { useState } from "react";
-import { useTheme } from "@emotion/react";
+import { useEffect, useState } from "react";
 import RoundedButton from "../components/global/RoundedButton";
+import Heading from "../components/global/Heading"
 import { navSections } from "../features/navigation";
+import { getStudentLatestLearningResults } from "../services/studentProfileService";
+import LoadingView from "./common/LoadingView";
+import ErrorView from "./common/ErrorView";
 
 export default function ProfileView() {
   const theme = useTheme();
+  const [error, setError] = useState();
   const Views = Object.freeze({ STUDENT: useEnumValue("STUDENT"), SETTINGS: useEnumValue("SETTINGS"), EDUCATOR: useEnumValue("EDUCATOR") });
   const [currentView, setCurrentView] = useState(Views.STUDENT);
   const viewDetails = currentView == Views.STUDENT ? "Profil ucznia" : currentView == Views.EDUCATOR ? "Profil edukatora" : "Ustawienia"
 
+  if (error)
+    return <ErrorView error={error} />
+
   return (
-    <NavLayout activeSectionIdOverride={navSections.profile}>
+    <NavLayout activeSectionIdOverride={navSections.profile} mode="flex" scroll>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Box>
-          <Typography fontFamily={"Baloo"} variant='h3'>Szymon - twój profil</Typography>
+          <Heading variant='h3'>Szymon - twój profil</Heading>
 
           <Typography variant="body1">{viewDetails}</Typography>
 
         </Box>
         <Box sx={{ display: "flex", gap: theme.spacing(4), alignItems: "center" }}>
           <RoundedButton label={"Uczeń"} active={currentView == Views.STUDENT} onClick={() => setCurrentView(Views.STUDENT)} />
-          <RoundedButton label={"Edukator"} active={currentView == Views.EDUCATOR} onClick={() => setCurrentView(Views.EDUCATOR)} />
+          {/* <RoundedButton label={"Edukator"} active={currentView == Views.EDUCATOR} onClick={() => setCurrentView(Views.EDUCATOR)} /> */}
           <RoundedButton label={"Ustawienia"} active={currentView == Views.SETTINGS} onClick={() => setCurrentView(Views.SETTINGS)} />
         </Box>
       </Box>
       {
-        currentView === Views.STUDENT ? <StudentProfileView />
+        currentView === Views.STUDENT ? <StudentProfileView setError={setError} />
           : currentView === Views.EDUCATOR ? <EducatorProfileView />
             : <ProfileSettingsView />
       }
@@ -37,13 +44,76 @@ export default function ProfileView() {
 }
 
 
-function StudentProfileView() {
-  return <></>
+function StudentProfileView({ setError }) {
+  const theme = useTheme();
+  const [loading, setLoading] = useState(true);
+  const [learningResults, setLearningResults] = useState(null);
+
+  console.log(loading);
+
+  useEffect(() => {
+    getStudentLatestLearningResults().then(learningResultsResponse => {
+      console.log(learningResultsResponse);
+      setError(learningResultsResponse.error);
+      setLearningResults(learningResultsResponse.data);
+      setLoading(false);
+    });
+  }, [])
+
+  if (loading)
+    return (<LoadingView embedded />);
+
+  return (
+    <Grid container flexGrow={1} gap={theme.spacing(4)} marginTop={theme.spacing(6)}>
+      <Grid item xs={6} sx={{ display: "flex", flexDirection: "column", alignItems: "stretch", gap: theme.spacing(2) }}>
+        <Heading variant="h4">Twoja ostatnia aktywność:</Heading>
+        {
+          learningResults.map(learningResult => (
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "stretch", gap: theme.spacing(2) }}>
+              <Divider flexItem orientation="horizontal" sx={{ marginBottom: theme.spacing(2) }} />
+              <Box sx={{ display: "flex", alignItems: "center", gap: theme.spacing(2) }}>
+                <Heading variant="h5">Rezultat nauczania</Heading>
+                <Typography color="grey" variant="caption">
+                  {new Date(learningResult.createdOn).toLocaleDateString()}
+                  {" "}
+                  {new Date(learningResult.createdOn).toLocaleTimeString()}
+                </Typography>
+              </Box>
+
+              {learningResult.assessments.map(assessment => {
+                let learningReq = learningResult.learningResourceDefinition.learningRequirements.filter(o => o.id === assessment.learningRequirementId)[0];
+                let qualifiedSubReqs = assessment.qualifiedSubRequirements.length;
+                let allSubReqs = learningReq.subRequirements.length;
+                return (
+                  <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <Typography>{learningReq.name}</Typography>
+                    <Box sx={{ display: "flex", gap: theme.spacing(4) }}>
+                      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: theme.spacing(2) }}>
+                        <CircularProgress variant="determinate" value={assessment.grade / 6 * 100} thickness={6} />
+                        <Typography>Ocena: {assessment.grade}</Typography>
+                      </Box>
+                      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: theme.spacing(2) }}>
+                        <CircularProgress variant="determinate" value={qualifiedSubReqs / allSubReqs * 100} thickness={6} color="secondary" />
+                        <Typography>Trudność</Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                );
+              })}
+            </Box>
+          ))
+        }
+      </Grid>
+      <Grid item xs={6}>
+
+      </Grid>
+    </Grid>
+  );
 }
 
 
 function EducatorProfileView() {
-  return <></>
+  return <>Not Implemented yet!</>
 }
 
 
