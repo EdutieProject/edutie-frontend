b8747bc244e9218e27df85d20b1ebb6225c0e3b2
sukerberk1 <szymon.l3232@gmail.com>
2024-09-27 18:19:12 +0200

    Add comments disclaimers

--- edutie-frontend/src/features/storage/activeLessonCache.js
+++ edutie-frontend/src/features/storage/activeLessonCache.js
@@ -1,4 +1,9 @@
-/** Active lesson caching made for learning result view redirection */
+/**
+ *  Active lesson caching made for learning result view redirection 
+ * 
+ * 
+ *  Not implemented as of 27.09.2024 due to different lesson possibilty errors
+*/
 export const noActiveLessonInCachePlacehodler = "no-active-lesson-in-cache";
 
 export const getActiveLessonId = () => localStorage.getItem("activeLessonId") ?? noActiveLessonInCachePlacehodler;
