8c35cef1cb08cb555406b3ff2f52b2d913c61b7a
sukerberk1 <szymon.l3232@gmail.com>
2024-12-08 21:55:32 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import ProfileView from "./views/ProfileView";
 import { navigationPath } from "./features/navigation/navigationPath";
 import React, { useEffect, useState } from "react";
 import MobileView from "./views/MobileView";
+import {SessionProvider} from "./features/session/SessionProvider";
 
 export default function App() {
   const [width, setWidth] = useState<number>(window.innerWidth);
@@ -40,21 +41,23 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <SelectedNavigationSectionProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path={navigationPath.home} element={<HomeView />} />
-            <Route path={navigationPath.lessonTree} element={<LessonTreeView />} />
-            <Route path={navigationPath.segmentTree} element={<SegmentTreeView />} />
-            <Route path={navigationPath.exercise} element={<LearningResourceView />} />
-            <Route path={navigationPath.account} element={<ProfileView />} />
-            <Route path={navigationPath.courses} element={<CoursesView />} />
-            <Route path={navigationPath.learningResult} element={<LearningResultView />} />
-            <Route path={navigationPath.creation} element={<LRDCreationView />} />
-          </Routes>
-        </BrowserRouter>
-      </SelectedNavigationSectionProvider>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <SessionProvider>
+          <SelectedNavigationSectionProvider>
+            <BrowserRouter basename={import.meta.env.VITE_BASE_PATH_OVERRIDE}>
+              <Routes>
+                <Route path={navigationPath.home} element={<HomeView/>}/>
+                <Route path={navigationPath.lessonTree} element={<LessonTreeView/>}/>
+                <Route path={navigationPath.segmentTree} element={<SegmentTreeView/>}/>
+                <Route path={navigationPath.exercise} element={<LearningResourceView/>}/>
+                <Route path={navigationPath.account} element={<ProfileView/>}/>
+                <Route path={navigationPath.courses} element={<CoursesView/>}/>
+                <Route path={navigationPath.learningResult} element={<LearningResultView/>}/>
+                <Route path={navigationPath.creation} element={<LRDCreationView/>}/>
+              </Routes>
+            </BrowserRouter>
+          </SelectedNavigationSectionProvider>
+        </SessionProvider>
+      </ThemeProvider>
   );
 }
