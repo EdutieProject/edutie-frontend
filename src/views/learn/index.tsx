7358efb3a3aa5e50375efd99e0a6eb3fac6c2323
sukerberk1 <szymon.l3232@gmail.com>
2025-04-01 13:38:12 +0200

    Add placeholder message for empty learning subjects
    Introduced a "Nothing to be seen here" placeholder with an expressionless emoji when no learning subjects are available. Updated the view to dynamically handle empty states and replaced hardcoded options in the autocomplete.

--- src/views/learn/index.tsx
+++ src/views/learn/index.tsx
@@ -7,9 +7,9 @@ import {ApiError, LearningSubject} from "src/services/types";
 import {getCreatedLearningSubjects} from "src/services/management/learningSubjectService";
 import LoadingView from "src/views/common/LoadingView";
 import ErrorView from "src/views/common/ErrorView";
-import {AdjustOutlined, RadioRounded} from "@mui/icons-material";
 import {useNavigate} from "react-router";
 import LearningSubjectIcon from "src/components/icons/LearningSubjectIcon";
+import straightFaceEmoji from "src/assets/svg/emoji/expressionless.svg";
 
 export default function LearnHomeView() {
     const theme = useTheme();
@@ -45,45 +45,59 @@ export default function LearnHomeView() {
             <Typography variant={"h3"} sx={{mt: 1}}><b>Let's learn.</b></Typography>
             <Autocomplete
                 disablePortal
-                options={[
-                    {label: 'The Shawshank Redemption', year: 1994},
-                    {label: 'The Godfather', year: 1972},
-                    {label: 'The Godfather: Part II', year: 1974},
-                    {label: 'The Dark Knight', year: 2008},
-                    {label: '12 Angry Men', year: 1957},
-                ]}
+                options={[]}
                 sx={{width: {xs: "100%", md: "80%", lg: "40%"}, my: 4}}
                 renderInput={(params) => <TextField {...params} label="What would you like to learn?"/>}
             />
             <Container maxWidth={"xl"}>
                 <Grid container width={"100%"} spacing={6}>
                     {
-                        learningSubjects.map(o => {
-                            return (
-                                <Grid size={{xs: 12, md: 4}}>
-                                    <Box sx={{
-                                        boxSizing: "border-box",
-                                        p: 4,
-                                        display: "flex",
-                                        borderRadius: 1,
-                                        border: "1px solid lightgray",
-                                        gap: 2,
-                                        width: "100%",
-                                        cursor: "pointer",
-                                        transition: "200ms ease",
-                                        "&:hover": {
-                                            boxShadow: theme.shadows[2]
-                                        }
-                                    }} onClick={() => navigate(navigationPath.fillPath(navigationPath.learningSubjectLearn, o.id))}>
-                                        <LearningSubjectIcon/>
-                                        <Box sx={{display: "flex", flexDirection: "column"}}>
-                                            <Typography variant={"h6"}>{o.name}</Typography>
-                                            <Typography variant={"subtitle1"} color={"textSecondary"}>Learning Subject</Typography>
+                        learningSubjects.length === 0 ? (
+                                <Box sx={{
+                                    width: "100%",
+                                    my: 2,
+                                    py: 4,
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    gap: 2
+                                }}>
+                                    <img src={straightFaceEmoji} alt={""} width={"100px"}/>
+                                    <Typography variant={"h4"} sx={{mb: 1, textAlign: "center"}}><b>Nothing to be seen
+                                        here.</b></Typography>
+                                    <Typography sx={{mb: 1, textAlign: "center"}}>Go to create tab to learn something
+                                        new!</Typography>
+                                </Box>
+                            ) :
+                            learningSubjects.map(o => {
+                                return (
+                                    <Grid size={{xs: 12, md: 4}}>
+                                        <Box sx={{
+                                            boxSizing: "border-box",
+                                            p: 4,
+                                            display: "flex",
+                                            borderRadius: 1,
+                                            border: "1px solid lightgray",
+                                            gap: 2,
+                                            width: "100%",
+                                            cursor: "pointer",
+                                            transition: "200ms ease",
+                                            "&:hover": {
+                                                boxShadow: theme.shadows[2]
+                                            }
+                                        }}
+                                             onClick={() => navigate(navigationPath.fillPath(navigationPath.learningSubjectLearn, o.id))}>
+                                            <LearningSubjectIcon/>
+                                            <Box sx={{display: "flex", flexDirection: "column"}}>
+                                                <Typography variant={"h6"}>{o.name}</Typography>
+                                                <Typography variant={"subtitle1"} color={"textSecondary"}>Learning
+                                                    Subject</Typography>
+                                            </Box>
                                         </Box>
-                                    </Box>
-                                </Grid>
-                            );
-                        })
+                                    </Grid>
+                                );
+                            })
                     }
 
                 </Grid>
